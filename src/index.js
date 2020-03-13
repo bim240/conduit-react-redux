1772d48013e9d4b5ea50e2c92850cfccf96abb76
bim240 <bimlendu357@gmail.com>
2020-03-13 16:33:01 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./component/app";
-import "./component/style.css";
+// import "./component/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
