9ce9cc252d29420792b3f53b5f77c4de6780e4e0
bim240 <bimlendu357@gmail.com>
2020-03-14 16:58:40 +0530

    buttons added

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { IoMdLogIn, IoMdLogOut } from "react-icons/io";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>header section</h1>
+      {/* <h1>header section</h1> */}
+      <div className="header_subcontainer">
+        {/* first icon */}
+        <div className="colorful_circle1">
+          <div className="colorful_circle2">
+            <div className="icon">
+              <IconContext.Provider value={{ className: "icon_shadow" }}>
+                <div>
+                  <FaHome />
+                </div>
+              </IconContext.Provider>
+            </div>
+          </div>
+        </div>
+        {/* login btn */}
+        <div className="colorful_circle1">
+          <div className="colorful_circle2">
+            <div className="icon">
+              <IconContext.Provider value={{ className: "icon_shadow" }}>
+                <div>
+                  <IoMdLogIn />
+                </div>
+              </IconContext.Provider>
+            </div>
+          </div>
+        </div>
+        {/* sign up ben*/}
+        <div className="colorful_circle1">
+          <div className="colorful_circle2">
+            <div className="icon">
+              <IconContext.Provider value={{ className: "icon_shadow" }}>
+                <div>
+                  <IoMdLogOut />
+                </div>
+              </IconContext.Provider>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
